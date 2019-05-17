de31638b0eb6d8f0b977d1d0620e7b847186b023
nathaniel <nathaniel.simard.42@gmail.com>
2019-05-16 21:40:21 -0400

    wip

--- coverage.sh
+++ coverage.sh
@@ -5,9 +5,11 @@ cargo install cargo-kcov
 docker run -it --rm --security-opt seccomp=unconfined \
 	-v "$HOME"/.cargo:/root/.cargo \
 	-v "$HOME"/.rustup:/root/.rustup \
-	-v "$PWD":/root/app kcov/kcov \
+	-v "$PWD":/root/app \
+	kcov/kcov:v35 \
 	sh -c "export PATH=$PATH:/root/.cargo/bin; \
 		apt update; \
 		apt install gcc -y; \
+		cd /root/app; \
 		cargo kcov"
 		

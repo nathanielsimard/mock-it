5345bc91cc684c276181aa95a74a31b151fbd9a2
nathaniel <nathaniel.simard.42@gmail.com>
2019-05-16 21:44:59 -0400

    Fix converage

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 mkdir -p target/cov
+cargo clean
 cargo install cargo-kcov
 docker run -it --rm --security-opt seccomp=unconfined \
 	-v "$HOME"/.cargo:/root/.cargo \
 	-v "$HOME"/.rustup:/root/.rustup \
 	-v "$PWD":/root/app \
-	kcov/kcov:v35 \
+	kcov/kcov:v36 \
 	sh -c "export PATH=$PATH:/root/.cargo/bin; \
 		apt update; \
 		apt install gcc -y; \
